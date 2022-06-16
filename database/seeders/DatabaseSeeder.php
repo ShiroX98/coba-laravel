bd7835c08b6b07ce20e9a2f99f3790272ebe40c9
ShiroX98 <yogasanyo27@gmail.com>
2022-06-16 16:05:36 +0800

    factory and faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,18 +16,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        User::create([
-            'name' => 'Shiro X',
-            'email' => 'shirox@gmail.com',
-            'password' => bcrypt('12345678'),
-        ]);
+        // User::create([
+        //     'name' => 'Shiro X',
+        //     'email' => 'shirox@gmail.com',
+        //     'password' => bcrypt('12345678'),
+        // ]);
 
-        User::create([
-            'name' => 'Doddy Ferdiansah',
-            'email' => 'doddy@gmail.com',
-            'password' => bcrypt('12345678'),
-        ]);
+        // User::create([
+        //     'name' => 'Doddy Ferdiansah',
+        //     'email' => 'doddy@gmail.com',
+        //     'password' => bcrypt('12345678'),
+        // ]);
+
+        User::factory(3)->create();
 
         Category::create([
             'name' => 'Web Programming',
@@ -39,41 +40,43 @@ class DatabaseSeeder extends Seeder
             'slug' => 'personal'
         ]);
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, ab ',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, ab aliquid nemo sunt voluptatem minus ut blanditiis nam iusto fuga quas beatae quisquam numquam quo cum, suscipit iure vel architecto officiis facere recusandae dolores autem alias. Itaque optio obcaecati quae temporibus error eum quam, cum, distinctio officiis officia aut sunt non pariatur possimus ad autem vel aliquam qui minus dolor suscipit neque. Deserunt temporibus est alias asperiores molestiae, modi aliquam placeat ex illo soluta beatae vero, incidunt aliquid, nostrum dignissimos.',
-            'category_id' => 1,
-            'user_id' => 1,
-        ]);
+        Post::factory(20)->create();
 
-        Post::create([
-            'title' => 'Judul Ke Dua',
-            'slug' => 'judul-ke-dua',
-            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, ab ',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, ab aliquid nemo sunt voluptatem minus ut blanditiis nam iusto fuga quas beatae quisquam numquam quo cum, suscipit iure vel architecto officiis facere recusandae dolores autem alias. Itaque optio obcaecati quae temporibus error eum quam, cum, distinctio officiis officia aut sunt non pariatur possimus ad autem vel aliquam qui minus dolor suscipit neque. Deserunt temporibus est alias asperiores molestiae, modi aliquam placeat ex illo soluta beatae vero, incidunt aliquid, nostrum dignissimos.',
-            'category_id' => 1,
-            'user_id' => 1,
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, ab ',
+        //     'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, ab aliquid nemo sunt voluptatem minus ut blanditiis nam iusto fuga quas beatae quisquam numquam quo cum, suscipit iure vel architecto officiis facere recusandae dolores autem alias. Itaque optio obcaecati quae temporibus error eum quam, cum, distinctio officiis officia aut sunt non pariatur possimus ad autem vel aliquam qui minus dolor suscipit neque. Deserunt temporibus est alias asperiores molestiae, modi aliquam placeat ex illo soluta beatae vero, incidunt aliquid, nostrum dignissimos.',
+        //     'category_id' => 1,
+        //     'user_id' => 1,
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ke Tiga',
-            'slug' => 'judul-ke-tiga',
-            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, ab ',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, ab aliquid nemo sunt voluptatem minus ut blanditiis nam iusto fuga quas beatae quisquam numquam quo cum, suscipit iure vel architecto officiis facere recusandae dolores autem alias. Itaque optio obcaecati quae temporibus error eum quam, cum, distinctio officiis officia aut sunt non pariatur possimus ad autem vel aliquam qui minus dolor suscipit neque. Deserunt temporibus est alias asperiores molestiae, modi aliquam placeat ex illo soluta beatae vero, incidunt aliquid, nostrum dignissimos.',
-            'category_id' => 2,
-            'user_id' => 1,
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Dua',
+        //     'slug' => 'judul-ke-dua',
+        //     'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, ab ',
+        //     'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, ab aliquid nemo sunt voluptatem minus ut blanditiis nam iusto fuga quas beatae quisquam numquam quo cum, suscipit iure vel architecto officiis facere recusandae dolores autem alias. Itaque optio obcaecati quae temporibus error eum quam, cum, distinctio officiis officia aut sunt non pariatur possimus ad autem vel aliquam qui minus dolor suscipit neque. Deserunt temporibus est alias asperiores molestiae, modi aliquam placeat ex illo soluta beatae vero, incidunt aliquid, nostrum dignissimos.',
+        //     'category_id' => 1,
+        //     'user_id' => 1,
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ke Empat',
-            'slug' => 'judul-ke-empat',
-            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, ab ',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, ab aliquid nemo sunt voluptatem minus ut blanditiis nam iusto fuga quas beatae quisquam numquam quo cum, suscipit iure vel architecto officiis facere recusandae dolores autem alias. Itaque optio obcaecati quae temporibus error eum quam, cum, distinctio officiis officia aut sunt non pariatur possimus ad autem vel aliquam qui minus dolor suscipit neque. Deserunt temporibus est alias asperiores molestiae, modi aliquam placeat ex illo soluta beatae vero, incidunt aliquid, nostrum dignissimos.',
-            'category_id' => 2,
-            'user_id' => 2,
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Tiga',
+        //     'slug' => 'judul-ke-tiga',
+        //     'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, ab ',
+        //     'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, ab aliquid nemo sunt voluptatem minus ut blanditiis nam iusto fuga quas beatae quisquam numquam quo cum, suscipit iure vel architecto officiis facere recusandae dolores autem alias. Itaque optio obcaecati quae temporibus error eum quam, cum, distinctio officiis officia aut sunt non pariatur possimus ad autem vel aliquam qui minus dolor suscipit neque. Deserunt temporibus est alias asperiores molestiae, modi aliquam placeat ex illo soluta beatae vero, incidunt aliquid, nostrum dignissimos.',
+        //     'category_id' => 2,
+        //     'user_id' => 1,
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Ke Empat',
+        //     'slug' => 'judul-ke-empat',
+        //     'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, ab ',
+        //     'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, ab aliquid nemo sunt voluptatem minus ut blanditiis nam iusto fuga quas beatae quisquam numquam quo cum, suscipit iure vel architecto officiis facere recusandae dolores autem alias. Itaque optio obcaecati quae temporibus error eum quam, cum, distinctio officiis officia aut sunt non pariatur possimus ad autem vel aliquam qui minus dolor suscipit neque. Deserunt temporibus est alias asperiores molestiae, modi aliquam placeat ex illo soluta beatae vero, incidunt aliquid, nostrum dignissimos.',
+        //     'category_id' => 2,
+        //     'user_id' => 2,
+        // ]);
     }
 }
 
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,7 +1,7 @@
 @extends("layouts.main")
 @section("container")
     <h2>{{ $post->title }}</h2>
-    <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+    <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
     {!! $post->body !!}
 
     <a href="/blog" class="d-block mt-3 text-decoration-none">Back to Posts   </a>
